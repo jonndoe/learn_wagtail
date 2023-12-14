6b983510303daa76437d5854ea64cafdd86568fe
jonndoe <lukichevvasily@gmail.com>
2023-12-14 20:26:34 +0300

    initial commit

---
+++ learn_wagtail/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from learn_wagtail.users.models import User
+from learn_wagtail.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
