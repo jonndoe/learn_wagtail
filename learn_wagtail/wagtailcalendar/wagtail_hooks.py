38651cc23852c3093a88980a73d047f4498b64b0
jonndoe <lukichevvasily@gmail.com>
2023-12-16 16:01:40 +0300

    Added calendar menu item in admin

--- learn_wagtail/wagtailcalendar/wagtail_hooks.py
+++ learn_wagtail/wagtailcalendar/wagtail_hooks.py
@@ -1,5 +1,6 @@
-from django.urls import path
+from django.urls import path, reverse
 from wagtail import hooks
+from wagtail.admin.menu import MenuItem
 
 from .views import index
 
@@ -9,3 +10,8 @@ def register_calendar_url():
     return [
         path("calendar/", index, name="calendar"),
     ]
+
+
+@hooks.register("register_admin_menu_item")
+def register_calendar_menu_item():
+    return MenuItem("Calendar", reverse("calendar"), icon_name="date")
