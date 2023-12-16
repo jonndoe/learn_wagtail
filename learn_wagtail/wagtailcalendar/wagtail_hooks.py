5348dd19edda2d439844375159be541bd74e6798
jonndoe <lukichevvasily@gmail.com>
2023-12-16 18:02:15 +0300

    Added group menu items in admin

--- learn_wagtail/wagtailcalendar/wagtail_hooks.py
+++ learn_wagtail/wagtailcalendar/wagtail_hooks.py
@@ -1,17 +1,25 @@
 from django.urls import path, reverse
 from wagtail import hooks
-from wagtail.admin.menu import MenuItem
+from wagtail.admin.menu import Menu, MenuItem, SubmenuMenuItem
 
-from .views import index
+from .views import index, month
 
 
 @hooks.register("register_admin_urls")
 def register_calendar_url():
     return [
         path("calendar/", index, name="calendar"),
+        path("calendar/month/", month, name="calendar-month"),
     ]
 
 
 @hooks.register("register_admin_menu_item")
 def register_calendar_menu_item():
-    return MenuItem("Calendar", reverse("calendar"), icon_name="date")
+    submenu = Menu(
+        items=[
+            MenuItem("Calendar", reverse("calendar"), icon_name="date"),
+            MenuItem("Current month", reverse("calendar-month"), icon_name="date"),
+        ]
+    )
+
+    return SubmenuMenuItem("Calendar", submenu, icon_name="date")
