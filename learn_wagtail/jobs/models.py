458fdf041151b84a6748884436fc990b465290d2
jonndoe <lukichevvasily@gmail.com>
2023-12-23 09:16:32 +0300

    Added models to jobs app

--- learn_wagtail/jobs/models.py
+++ learn_wagtail/jobs/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from wagtail.admin.panels import FieldPanel
+from wagtail.fields import RichTextField
+from wagtail.models import Page
+from wagtail.search import index
 
-# Create your models here.
+
+class LocationPage(Page):
+    intro = RichTextField(blank=True)
+    content_panels = Page.content_panels + [FieldPanel("intro", classname="full")]
+    subpage_type = ["SubLocationPage"]
+
+
+class SubLocationPage(Page):
+    intro = RichTextField(blank=True)
+    content_panels = Page.content_panels + [FieldPanel("intro", classname="full")]
+    subpage_type = ["JobPage"]
+
+
+class JobsIndexPage(Page):
+    intro = RichTextField(blank=True)
+    content_panels = Page.content_panels + [FieldPanel("intro", classname="full")]
+    subpage_types = ["JobPage"]
+
+
+class JobPage(Page):
+    locationcode = models.CharField(max_length=20)
+    locationname = models.CharField(max_length=250)
+    jobcode = models.CharField(max_length=10, blank=True)
+    datedue = models.DateField("Next due date")
+    datedone = models.DateField("Last done date")
+    jobtitle = models.CharField(max_length=60, blank=True)
+    intro = models.CharField(max_length=250, blank=True)
+    body = models.TextField(blank=True)
+    subsystem = models.CharField(max_length=120, blank=True)
+    vessel_comment = RichTextField(blank=True)
+    responsible_crew = models.CharField(max_length=30, blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField("intro"),
+        index.SearchField("body"),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel("locationname"),
+        FieldPanel("locationcode"),
+        FieldPanel("datedone"),
+        FieldPanel("datedue"),
+        FieldPanel("intro"),
+        FieldPanel("body", classname="full"),
+    ]
+
+    subpage_types = ["JobCardPage"]
+
+
+class JobCardPage(Page):
+    datedone = models.DateField("Last done date")
+    datedue = models.DateField("Next due date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField("intro"),
+        index.SearchField("body"),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel("datedone"),
+        FieldPanel("datedue"),
+        FieldPanel("intro"),
+        FieldPanel("body", classname="full"),
+    ]
